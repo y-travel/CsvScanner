cbc2f8814b559e54853e198a0869456370886703
Majid <majid.taheri@yandex.com>
2021-01-11 14:41:21 +0330

    update `tsconfig.json`

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,6 +5,7 @@ export class Config{
  csvFilePaths: string[];
     presetFilePaths: string[];
     serverPort: string;
+    jobFilePaths: string[];
  /**
   *
   */
@@ -15,14 +16,22 @@ export class Config{
  async loadFromFile(xmlFilePath:string){   
     const contents=await  readFile(xmlFilePath,{encoding:'utf-8'})
     const doc=parseXmlString(contents);
+
     this.csvFilePaths =  Array.from(  doc.find('//CsvFiles'))
         .map(elm=>elm.attr('Path'))
         .filter(Boolean)
         .map(attr=>attr.value());
+
     this.presetFilePaths =  Array.from(  doc.find('//PresetFiles'))
         .map(elm=>elm.attr('Path'))
         .filter(Boolean)
         .map(attr=>attr.value());
+
+    this.jobFilePaths =  Array.from(  doc.find('//JobFiles'))
+        .map(elm=>elm.attr('Path'))
+        .filter(Boolean)
+        .map(attr=>attr.value());
+
     const serverElm=doc.get('//Server');
     [this.serverPort] = [serverElm.attr('Port')].filter(Boolean).map(attr=>attr.value());
  }
