54f8288e011c97d6155db53e4719560b30b59c3f
Majid <majid.taheri@yandex.com>
2021-01-11 15:05:17 +0330

    `AppService`

--- src/backend/home/app.service.ts
+++ src/backend/home/app.service.ts
@@ -5,7 +5,7 @@ import { detectFileEncoding } from '../../lib/text-encoding';
 import { globFiles } from '../../lib/utils';
 import * as uuid from 'uuid'
 import { stat } from 'fs-extra';
-import * as humanSize from 'src/backend/file/node_modules/human-size'
+import * as humanSize from 'human-size'
 @Injectable()
 export class AppService {
 	config: Config;
@@ -22,23 +22,5 @@ export class AppService {
 	getHello(): string {
 		return 'Hello World!';
 	}
-	accquireFileId(filePath) {
-		const id = this.idByFilePath[filePath];
-		if (id) return id;
-		this.idByFilePath[filePath] = uuid.v1();
-		return this.idByFilePath[filePath];
-	}
-	async getFiles() {
-		const files = await globFiles(this.config.csvFilePaths);
-		return Promise.all(files.map(async filePath => {
-			const id = this.accquireFileId(filePath);
-			const textEncoding = await detectFileEncoding(filePath);
-			const baseName = path.basename(filePath);
-			const fileStat=await  stat(filePath);
-			this.fileDataById[id]={ id,size:fileStat.size ,humanSize:humanSize(fileStat.size,1), filePath, textEncoding, baseName };
-			return this.fileDataById[id];
-		}));
-		
-	}
-	getFile(id){ return this.fileDataById[id] }
+	  
 }
--- src/lib/preset.ts
+++ src/lib/preset.ts
@@ -1,62 +1,100 @@
 import { readFile } from 'fs-extra'
-import { parseXmlString,Element} from 'libxmljs' 
-export class Preset{
-    processRow(item: any):CategoryPreset[] {
-    return this.categories.filter( cat=>cat.checkItem(item));
+import * as validators from './validators';
+import { parseXmlString, Element } from 'libxmljs'
+export class Preset {
+    filePath: any;
+    processRow(item: any): CategoryPreset[] {
+        return this.categories.filter(cat => cat.checkItem(item));
     }
-    categories:CategoryPreset[];
+    categories: CategoryPreset[];
     /**
      *
      */
     constructor() {
-        
+
     }
-    async loadFromFile(filePath){
-        const contents=await  readFile(filePath,{encoding:'utf-8'})
-        const doc=parseXmlString(contents);
-        this.categories =  Array.from(  doc.find('//Category')).map(
-            elm=>new CategoryPreset(elm)
-        ); 
-        this.categories.unshift(new CategoryPresetForAllData());
+    async loadFromFile(filePath) {
+        const contents = await readFile(filePath, { encoding: 'utf-8' });
+        this.filePath = filePath;
+        try {
+            const doc = parseXmlString(contents);
+            this.categories = Array.from(doc.find('//Category')).map(
+                elm => new CategoryPreset(elm)
+            );
+            this.categories.unshift(new CategoryPresetForAllData());
+        } catch (exc) {
+            throw exc;
+        }
     }
 }
-class CategoryPreset{
-    attributes: { name: string;icon:string;color:string;limit:string };
+class CategoryPreset {
+    attributes: { name: string; icon: string; color: string; limit:number };
+    rules: Array<{func:Function,functionName:string,argv:any,sourceField:string}>;
     /**
      *
      */
-    constructor(elm:Element ) {
-        function getValue(attributeName){
-            const attr=elm.attr(attributeName);
+    constructor(elm: Element) {
+        function getValue(attributeName):any {
+            const attr = elm.attr(attributeName);
             return attr ? attr.value() : '';
         }
-        if(!elm) return;
-          this.attributes={   
-             name:getValue('Name'),
-             icon: getValue('Icon'),
-             color: getValue('Color'),
-             limit:getValue('Limit')   
-         };
-
+        if (!elm) return;
+        this.attributes = {
+            name: getValue('Name'),
+            icon: getValue('Icon'),
+            color: getValue('Color'),
+            limit: getValue('Limit')  
+        };
+        this.attributes.limit=this.attributes.limit ? parseInt(this.attributes.limit as any): undefined;
+        this.rules =   elm.childNodes().map((ruleElm:Element) =>{
+            if(ruleElm.name()!='Rule') return ;
+            const result :CategoryPreset['rules'][0] = Object.fromEntries(ruleElm.attrs().map(attr=>[ 
+                camelCase(attr.name()),attr.value()])) as any;
+            const func:Function= validators[result.functionName];
+            if(!(func instanceof Function)){
+                return; // TODO : ERROR
+            }
+            if(   func['numberInput'] && typeof result.argv=='string'){
+                result.argv=parseInt(result.argv.replace(/[_,]/ig,'')); 
+            }
+            result.func=func;
+            
+            return result;
+        }).filter(Boolean);
     }
-     
-    canIndex({lineIndex}){
+
+    canIndex({ lineIndex }) {
         return true;
     }
-    public checkItem(item){
-
-    }   
+    public checkItem(item) {
+        let score=0;
+        for(const rule of this.rules){
+            const cellValue=item[rule.sourceField];
+            if((cellValue ===undefined)){
+                continue ; //error
+            }
+            if(!rule.func(cellValue,rule.argv))  return false;
+            score++;     
+        }  
+        return score>0; 
+    }
 }
-
+function camelCase(s:string){
+    return s[0].toLocaleLowerCase()+s.substr(1);
+}
 class CategoryPresetForAllData extends CategoryPreset {
     /**
      *
      */
     constructor() {
         super(null);
+        this.attributes = { name: 'AllData' } as any;
 
     }
-    public checkItem(item){
-        return true;
+    public checkItem(item) {
+        return true; 
+    }
+    canIndex({ lineIndex }) {
+        return lineIndex % 100 == 0;
     }
 }
