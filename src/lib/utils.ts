61c12ddca220e4d59d92aa074e8d062f5c5dfc1c
Majid <majid.taheri@yandex.com>
2021-01-11 14:49:32 +0330

    update utils for adding asyncDelay & code format

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,12 @@
 import * as glob0 from 'glob';
-import {promisify} from 'util';
-const glob=promisify(glob0);
-export function globFiles(dirs:string|string[]) {  
-    dirs= dirs instanceof Array ? dirs : [dirs];
-    return Promise.all(dirs.map(dir=>glob(dir)) ).then((results:Array<string[]>)=>
-       [''].concat(...results).filter(Boolean)
-        ) ;
+import { promisify } from 'util';
+const glob = promisify(glob0);
+export function globFiles(dirs: string | string[]) {
+  dirs = dirs instanceof Array ? dirs : [dirs];
+  return Promise.all(dirs.map(dir => glob(dir))).then((results: Array<string[]>) =>
+    [''].concat(...results).filter(Boolean)
+  );
+}
+export function asyncDelay(ms, value?) {
+  return new Promise((resolve, reject) => setTimeout(resolve, ms, value));
 }
